e8407768437c81261f3bcdc82d073a9dafd893be
Seth Axen <seth.axen@gmail.com>
2022-03-10 13:19:29 +0100

    Add empty polynomial regression notebook

---
+++ polynomial_regression.jl
@@ -0,0 +1,28 @@
+### A Pluto.jl notebook ###
+# v0.18.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 1c12c228-a06c-11ec-04e6-c5df312b35b9
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═1c12c228-a06c-11ec-04e6-c5df312b35b9
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
