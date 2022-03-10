c5f2b86a690ac1264f1de05a6e8818983615f24c
Documenter.jl <documenter@juliadocs.github.io>
2022-03-10 12:18:22 +0000

    build based on ca3931a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = IntroML","category":"page"},{"location":"#IntroML","page":"Home","title":"IntroML","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for IntroML.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [IntroML]","category":"page"}]
+}
