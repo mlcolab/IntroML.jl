a9670a54ae2bcfe55a183052c434a5d84728c520
Seth Axen <seth@sethaxen.com>
2023-06-14 16:53:16 +0200

    Simplify notebook building code

--- docs/make.jl
+++ docs/make.jl
@@ -6,26 +6,19 @@ const DOCS_PATH = @__DIR__
 const SRC_PATH = joinpath(DOCS_PATH, "src")
 const NB_PATH = joinpath(dirname(DOCS_PATH), "notebooks")
 
-function build_notebook(nbpath, outpath)
-    htmlpath = joinpath(outpath, first(splitext(nbpath)) * ".html")
-    @info "Building notebook at $nbpath to HTML file at $htmlpath"
-    PlutoSliderServer.export_notebook(
-        nbpath;
-        Export_output_dir=outpath,
+function build_notebooks(dir_path, out_path)
+    PlutoSliderServer.export_directory(
+        dir_path;
+        Export_output_dir=out_path,
         Precompute_enabled=true,
         Precompute_max_filesize_per_group=1e9,
         Export_create_index=false,
     )
-    isfile(htmlpath) ||
-        @warn "Failed to build notebook at $nbpath to HTML file at $htmlpath"
-    return htmlpath
+    return out_path
 end
 
 # build Pluto notebooks
-for fn in readdir(NB_PATH)
-    nbpath = joinpath(NB_PATH, fn)
-    build_notebook(nbpath, SRC_PATH)
-end
+build_notebooks(NB_PATH, SRC_PATH)
 
 DocMeta.setdocmeta!(IntroML, :DocTestSetup, :(using IntroML); recursive=true)
 
