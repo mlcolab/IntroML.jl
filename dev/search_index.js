e00fef2beec85822aa07f5d8cca8f542f07c8072
Documenter.jl <documenter@juliadocs.github.io>
2023-11-28 13:14:53 +0000

    delete history

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = IntroML","category":"page"},{"location":"#Introduction-to-Machine-Learning","page":"Home","title":"Introduction to Machine Learning","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Materials for the Introduction to Machine Learning workshop with the Machine Learning ⇌ Science Colaboratory.","category":"page"},{"location":"","page":"Home","title":"Home","text":"Supervised Learning: One step at a time","category":"page"}]
+}
---
+++ versions.js
@@ -0,0 +1,7 @@
+var DOC_VERSIONS = [
+  "stable",
+  "v2023.7",
+  "dev",
+];
+var DOCUMENTER_NEWEST = "2023.07.14";
+var DOCUMENTER_STABLE = "stable";
